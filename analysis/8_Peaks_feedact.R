47241df309d1b6764d355765a32ddfb5f3d54043
Cml_Magneville <camille.magneville@gmail.com>
2023-01-30 17:48:42 +0100

    :green_heart: New analysis 5 biggest events

--- analysis/8_Peaks_feedact.R
+++ analysis/8_Peaks_feedact.R
@@ -1,10 +1,8 @@
 ################################################################################
 ##
-## Script to compute temporal turnover between time slots per feedact and ...
-## ... plot the turnover pairs (timeslot 1-2, 1-3, 2-3) for both days ...
-## ... on each site
+## Script to study the five biggest events (sequence) for each feeding activity
 ##
-## 7_Temporal_seq_turnover.R
+## 8_Peaks_feedact.R
 ##
 ## 30/01/2023
 ##
@@ -13,8 +11,8 @@
 ################################################################################
 
 
-# 1 - Load data gathered at the sequence level for each day and traits data ####
 
+# 1 - Load data:
 
 bites_seq_03_df <- readRDS(here::here("transformed_data",
                                       "presabs_seq_03.rds"))
@@ -39,12 +37,12 @@ bites_seq_df <- bites_seq_03_df
 # coral
 sp_feedact_nm_vect <- sp_diet$Latin_nm[which(sp_diet$Diet_Parravicini_2020 == "Corallivores")]
 coral_bites_seq_03_df <- subset.df.feedact(bites_seq_df,
-                                        sp_feedact_nm_vect)
+                                           sp_feedact_nm_vect)
 
 # herb
 sp_feedact_nm_vect <- sp_diet$Latin_nm[which(sp_diet$Diet_Parravicini_2020 == "Herbivores Microvores Detritivores")]
 herb_bites_seq_03_df <- subset.df.feedact(bites_seq_df,
-                                        sp_feedact_nm_vect)
+                                          sp_feedact_nm_vect)
 
 # invert
 sp_feedact_nm_vect <- sp_diet$Latin_nm[which(sp_diet$Diet_Parravicini_2020 %in%
@@ -52,12 +50,12 @@ sp_feedact_nm_vect <- sp_diet$Latin_nm[which(sp_diet$Diet_Parravicini_2020 %in%
                                                  "Microinvertivores",
                                                  "sessile invertivores"))]
 invert_bites_seq_03_df <- subset.df.feedact(bites_seq_df,
-                                        sp_feedact_nm_vect)
+                                            sp_feedact_nm_vect)
 
 # crust
 sp_feedact_nm_vect <- sp_diet$Latin_nm[which(sp_diet$Diet_Parravicini_2020 == "Crustacivores")]
 crust_bites_seq_03_df <- subset.df.feedact(bites_seq_df,
-                                       sp_feedact_nm_vect)
+                                           sp_feedact_nm_vect)
 
 
 ## 04
@@ -144,28 +142,10 @@ crust_bites_seq_06_df <- subset.df.feedact(bites_seq_df,
                                            sp_feedact_nm_vect)
 
 
-# 3 - Compute temporal turnover between timeslots and plot them ####
-# Add together the bites of sequences from a given timeslot to get bites/timeslot
-# For each feedact, get one dataframe with the values for the two sites and ...
-# ... two days and one plot per feedact
+# 3 - Get the five strongest event / site for each feeding activity (days pooled) ####
+# I thus get 10 sequences per feeding activity (5 per site)
 
 
-# Corallivory:
-list_df <- list(coral_bites_seq_03_df, coral_bites_seq_04_df,
-             coral_bites_seq_05_df, coral_bites_seq_06_df)
-turn_coral_df <- compute.temporal.turn(list_df)
+#
 
-# Herbivory:
-list_df <- list(herb_bites_seq_03_df, herb_bites_seq_04_df,
-                herb_bites_seq_05_df, herb_bites_seq_06_df)
-turn_herb_df <- compute.temporal.turn(list_df)
 
-# Invertivory:
-list_df <- list(invert_bites_seq_03_df, invert_bites_seq_04_df,
-                invert_bites_seq_05_df, invert_bites_seq_06_df)
-turn_invert_df <- compute.temporal.turn(list_df)
-
-# Crustacivory:
-list_df <- list(crust_bites_seq_03_df, crust_bites_seq_04_df,
-                crust_bites_seq_05_df, crust_bites_seq_06_df)
-turn_crust_df <- compute.temporal.turn(list_df)
